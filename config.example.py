b44486b0577da11644bf1ed9463f301785c26ca3
Jolon Behrent <jolon.behrent@gmail.com>
2024-12-20 22:38:55 +1300

    Add HTML formatting for emails
    Also add extra configuration options

--- config.example.py
+++ config.example.py
@@ -16,6 +16,9 @@ email = {
     "port": 465,  # this can normally be left at 465
 }
 
+# Don't send emails, but print results to terminal if True
+DRY_RUN = True
+
 # You should enable this depending on your CPU
 # A dense (i.e. very few excludes) 8 person graph will take several minutes
 # If is recommened that you set this to True for a graph of that size or larger
@@ -29,5 +32,11 @@ min_grouping = 2
 # Similar to the min_grouping value above: True -> min_grouping=2, False -> min_grouping=3
 allow_pairs = True
 
-# Don't send emails, but print results to terminal if True
-DRY_RUN = True
+## Optional email parameters
+
+# If you have some place (such as Google Drive) where you allow people to track gift ideas, enter the link here
+# When users are emailed, they will all receive a link/button in the email
+gift_ideas = ""
+
+# Enter a value here for the maximum budget. You should enter the currency symbol too, e.g. "$50", "£40", etc.
+budget = ""
